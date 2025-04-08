5d6f38ed6ed02ad6caa60a9985fd195b6c4189f4
georgyogolla <georgyogolla@gmail.com>
2025-04-08 17:32:04 +0300

    Introduction to javascript variables and data types

---
+++ javascript/data-types/number.js
@@ -0,0 +1,5 @@
+// JavaScript has ONE Number type
+// Positive numbers!
+// Negative numbers!
+// Whole number (integers)!
+// Decimal numbers!
---
+++ javascript/variables/variables.js
@@ -0,0 +1,22 @@
+// VARIABLES
+// Variables are like "labeled jars"
+// for a value in JavaScript.
+// We can store a value and give it a
+// name, so that we can...
+// - recall it
+// - use it
+// - or change it later on.
+
+// Declaring variables(syntax)
+// let someName = value 
+
+// Recall variables
+let numOne = 3;
+let numTwo = 4;
+console.log(numOne + numTwo);
+
+// Updating variables
+console.log(numOne - 1);
+console.log(numOne);
+console.log(numOne = numOne - 1);
+console.log(numOne);
