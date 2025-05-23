eff4dec722b51a493adf4db4afa469bd2db5c4ee
georgyogolla <georgyogolla@gmail.com>
2025-05-23 12:47:40 +0300

    Introductio to Arrays

---
+++ arrays/arrays.js
@@ -0,0 +1,10 @@
+// The Array object, as with arrays in other programming languages, 
+// enables storing a collection of multiple items under a single variable name, 
+// and has members for performing common array operations.
+
+const firstArray = [8, "hello", null, true, undefined, NaN, 10, 'Kenya', false, "World"];
+
+console.log(firstArray);
+console.log(firstArray[3]);
+console.log(firstArray.length);
+console.log(firstArray[firstArray.length - 1]);
