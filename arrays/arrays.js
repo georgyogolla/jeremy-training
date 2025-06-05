68b42bb99ad5d9b402dac94a9cd99fa714c493c1
georgyogolla <georgyogolla@gmail.com>
2025-06-05 22:53:34 +0300

    array methods that do not require callbacks

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -20,3 +20,36 @@ fruits.length = 3;
 console.log(fruits.length);
 console.log(fruits);
 
+console.log(Object.keys(fruits));
+console.log(Object.values(fruits));
+
+// Array.isArray(): Checks if a value is an array.
+console.log(Array.isArray(fruits));
+const drinks = ['soft drinks', 'hard drinks'];
+
+// concat(): Merges two or more arrays.
+const combined = fruits.concat(drinks);
+console.log(combined);
+
+// flat(): Creates a new array with all sub-array elements concatenated into it recursively up to the specified depth.
+const mixedArrs = [1, 2, ['color', true], ['undefined', 'hello']];
+const flattened = mixedArrs.flat();
+console.log(flattened);
+
+// includes(): Determines whether an array includes a certain value among its entries, returning true or false.
+console.log(fruits.includes('apple'));
+
+// indexOf(): Returns the first index at which a given element can be found in the array, or -1 if it is not present.
+console.log(flattened.indexOf('hellooooooo'));
+
+// pop(): Removes the last element from an array and returns that element. 
+console.log(flattened.pop());
+
+// shift(): Removes the first element from an array and returns that removed element. 
+console.log(flattened.shift());
+
+// unshift(): Adds one or more elements to the beginning of an array and returns the new length of the array. 
+console.log(flattened.unshift('cars', 200, ['javascript', 'html', 'css']));
+console.log(flattened)
+
+
