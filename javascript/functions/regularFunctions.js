a6f906a563d904fe92fe1caa0ccdc5211d00d860
georgyogolla <georgyogolla@gmail.com>
2025-08-26 22:55:05 +0300

    Introduction to functions

---
+++ javascript/functions/regularFunctions.js
@@ -0,0 +1,11 @@
+// Declaring javascript regular functions
+function greet(){
+    console.log("Hello Wolrd!!");
+};
+greet();
+
+// functions arguments and parameters
+function greetSomeone(name, age){
+    console.log(`Hello, my name is ${name} and I am ${age} years old!`);
+}
+greetSomeone("Jeremy", 20);
