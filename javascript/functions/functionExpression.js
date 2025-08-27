d9f06283646ad95330041486f72a892c9e883610
georgyogolla <georgyogolla@gmail.com>
2025-08-27 09:48:53 +0300

    function expression

---
+++ javascript/functions/functionExpression.js
@@ -0,0 +1,6 @@
+// Function Expressions
+// In JavaScript, a function expression is a way to store functions in variables. For example,
+let square = function(n){
+    return n * n;
+};
+console.log(square(4));
--- javascript/functions/regularFunctions.js
+++ javascript/functions/regularFunctions.js
@@ -9,3 +9,37 @@ function greetSomeone(name, age){
     console.log(`Hello, my name is ${name} and I am ${age} years old!`);
 }
 greetSomeone("Jeremy", 20);
+greetSomeone("Joshua", 25);
+
+// function that adds two numbers
+console.log("---ADD TWO NUMBERS---");
+// function add(num1, num2){
+//     let sum = num1 + num2;
+//     console.log(`Sum of ${num1} and ${num2} is : ${sum}`);
+// };
+// add(20, 45);
+
+function add(num1, num2){
+    // let sum = num1 + num2;
+    console.log(`Sum of ${num1} and ${num2} is : ${num1 + num2}`);
+    // return (`Sum of ${num1} and ${num2} is : ${num1 + num2}`);
+};
+add(20, 45);
+
+// return statement in javascript
+function square(x){
+    return x * x;
+};
+const squared = square(4);
+console.log(squared);
+
+// The return Statement Terminates the Function
+// Any code written in the function after the return statement is not executed. For example,
+console.log("--- Terminating function execution using return statement---");
+function terminatingExecution(){
+    console.log("This is the first exeution");
+    return "This where execution is terminated";
+    console.log("This won't be executed"); 
+}
+const termination = terminatingExecution();
+console.log(termination);
