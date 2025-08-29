c1197d759b0c0fd6d5a772df5b0f82227381780a
georgyogolla <georgyogolla@gmail.com>
2025-08-29 09:48:11 +0300

    nesting functions

--- javascript/functions/functionExpression.js
+++ javascript/functions/functionExpression.js
@@ -4,3 +4,4 @@ let square = function(n){
     return n * n;
 };
 console.log(square(4));
+
--- javascript/functions/regularFunctions.js
+++ javascript/functions/regularFunctions.js
@@ -43,3 +43,45 @@ function terminatingExecution(){
 }
 const termination = terminatingExecution();
 console.log(termination);
+
+// When you pass an object as a parameter, if the function changes the object's properties, that change is visible outside the function,
+//  as shown in the following example:
+
+function passObject(obj){
+    return obj.color = "Yellow";
+};
+
+const drink = {
+    make: "Coke",
+    color: "Black",
+    Price: "Ksh. 60"
+}
+console.log(`The color of the drink is: ${drink.color}`);
+console.log(passObject(drink));
+
+// When you pass an array as a parameter,
+//  if the function changes any of the array's values, 
+// that change is visible outside the function, as shown in the following example:
+
+function modifyArr(arr){
+    console.log(arr.push(true));
+    console.log(arr);
+    return arr.push(false); 
+}
+
+const dataTypes = ['undefined'];
+console.log(dataTypes[0]);
+console.log(modifyArr(dataTypes));
+console.log(dataTypes);
+
+// Function declarations and expressions can be nested, 
+// which forms a scope chain. For example:
+
+function addSquares(a, b){
+    function square(x){
+        return x * x;
+    }
+    return square(a) + square(b);
+}
+const sum = addSquares(2, 4);
+console.log(sum);
